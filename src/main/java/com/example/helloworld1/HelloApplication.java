d25f8fc6b62fbec38942eed36dd639051214729b
GiangLe13 <ltngvenus@gmail.com>
2024-01-12 22:00:05 +0100

    remove @EnableJpaRep to fix BeanCreationError

--- src/main/java/com/example/helloworld1/HelloApplication.java
+++ src/main/java/com/example/helloworld1/HelloApplication.java
@@ -2,12 +2,10 @@ package com.example.helloworld1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableWebMvc
-@EnableJpaRepositories
 public class HelloApplication {
 
     public static void main(String[] args) {
